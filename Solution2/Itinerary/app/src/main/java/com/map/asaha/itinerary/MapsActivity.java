b2afc2ec3a5f7894418402d98f9f3b36dfccf09f
asaha1 <asaha@ncsu.edu>
2016-02-24 16:41:04 -0500

    Finally pushing the solution 2 to master repo

---
+++ Solution2/Itinerary/app/src/main/java/com/map/asaha/itinerary/MapsActivity.java
@@ -0,0 +1,94 @@
+package com.map.asaha.itinerary;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.UrlTileProvider;
+
+
+import java.io.IOException;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        setUpMapIfNeeded();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    private void setUpMapIfNeeded() {
+        if (mMap == null) {
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+        }
+        if (mMap != null)
+            setUpMap();
+    }
+
+    public void onSearch(View view) throws IOException {
+        String location_1 = ((EditText) findViewById(R.id.place_1)).getText().toString();
+        String location_2 = ((EditText) findViewById(R.id.place_2)).getText().toString();
+
+        if (!location_1.equals("") && !location_2.equals("")) {
+            LatLng latLng1 = MapUtility.getLatLngFromLocationName(location_1);
+            mMap.addMarker(new MarkerOptions().position(latLng1).title(location_1));
+
+            LatLng latLng2 = MapUtility.getLatLngFromLocationName(location_2);
+            mMap.addMarker(new MarkerOptions().position(latLng2).title(location_2));
+
+            MapUtility.drawPath(mMap, latLng1, latLng2);
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng1, 10));
+        }
+    }
+
+    private void setUpMap() {
+//        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap.setOnMyLocationChangeListener(myLocationChangeListener);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+    }
+
+    private GoogleMap.OnMyLocationChangeListener myLocationChangeListener = new GoogleMap.OnMyLocationChangeListener() {
+        @Override
+        public void onMyLocationChange(Location location) {
+            LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(loc));
+            if (mMap != null) {
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 16.0f));
+            }
+        }
+    };
+}
