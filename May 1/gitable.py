882b8b72256bd03669601b3d7fdc763ed90092aa
yemao616 <ymao4@ncsu.edu>
2016-04-24 00:25:41 +0000

    add features

--- May 1/gitable.py
+++ May 1/gitable.py
@@ -24,6 +24,7 @@ import urllib2
 import json
 import re,datetime
 import sys
+import pprint
  
 class L():
   "Anonymous container"
@@ -46,8 +47,8 @@ def secs(d0):
   delta = d - epoch
   return delta.total_seconds()
  
-def dump1(u,issues):
-  token = "f5301f854450c086d7b523ac8119e0b46444d3c9" # <===
+def dump1(u,issues, token):
+  
   request = urllib2.Request(u, headers={"Authorization" : "token "+token})
   v = urllib2.urlopen(request).read()
   w = json.loads(v)
@@ -72,22 +73,99 @@ def dump1(u,issues):
     issues[issue_id] = all_events
   return True
 
-def dump(u,issues):
+def dumpMilestone(u,milestones,token):
   try:
-    return dump1(u, issues)
+    return dumpMilestone1(u, milestones,token)
+  except urllib2.HTTPError as e:
+    if e.code != 404:
+      print(e)
+      print("404 Contact TA")
+    return False
+  except Exception as e:
+    print(u)
+    print(e)
+    print("other Contact TA")
+    return False
+
+
+def dumpMilestone1(u, milestones, token):
+  request = urllib2.Request(u, headers={"Authorization" : "token "+token})
+  v = urllib2.urlopen(request).read()
+  w = json.loads(v)
+  if not w or ('message' in w and w['message'] == "Not Found"): return False
+  milestone = w
+  identifier = milestone['id']
+  milestone_id = milestone['number']
+  milestone_title = milestone['title']
+  milestone_description = milestone['description']
+  created_at = secs(milestone['created_at'])
+  due_at_string = milestone['due_on']
+  due_at = secs(due_at_string) if due_at_string != None else due_at_string
+  closed_at_string = milestone['closed_at']
+  closed_at = secs(closed_at_string) if closed_at_string != None else closed_at_string
+  user = milestone['creator']['login']
+    
+  milestoneObj = L(ident=identifier,
+               m_id = milestone_id,
+               m_title = milestone_title,
+               m_description = milestone_description,
+               created_at=created_at,
+               due_at = due_at,
+               closed_at = closed_at,
+               user = user)
+  milestones.append(milestoneObj)
+  return True
+
+def dump(u,issues, token):
+  try:
+    return dump1(u, issues, token)
   except Exception as e: 
     print(e)
     print("Contact TA")
     return False
 
 def launchDump():
+  token = "8ee15c36451200b4d01e520da2c4ea0c964808ab" # <=== arnab's token
+  nameNum = 1
+  nameMap = dict()
+
+  milestoneNum = 1
+  milestoneMap = dict()
+
+  page = 1
+  milestones = []
+  
+  while(True):
+    url = 'https://api.github.com/repos/arnabsaha1011/milestones/' + str(page)
+    doNext = dumpMilestone(url, milestones, token)
+    print("milestone "+ str(page))
+    page += 1
+    if not doNext : break
+  
   page = 1
   issues = dict()
   while(True):
-    doNext = dump('https://api.github.com/repos/arnabsaha1011/mypackse/issues/events?page=' + str(page), issues)
+    doNext = dump('https://api.github.com/repos/arnabsaha1011/mypackse/issues/events?page=' + str(page), issues, token)
     print("page "+ str(page))
     page += 1
     if not doNext : break
+  
+  issueTuples = []
+  eventTuples = []
+  milestoneTuples = []
+  commentTuples = []
+  commitTuples = []
+  
+  for milestone in milestones:
+    if not milestone.user in nameMap:
+      nameMap[milestone.user] = nameNum
+      nameNum+=1
+    milestoneMap[milestone.m_title] = milestone.m_id
+    milestoneTuples.append([milestone.m_id, milestone.m_title, milestone.m_description, milestone.created_at, milestone.due_at, milestone.closed_at, nameMap[milestone.user], milestone.ident])
+  
+  pprint.pprint(milestoneTuples);
+  print("");
+  
   for issue, events in issues.iteritems():
     print("ISSUE " + str(issue))
     for event in events: print(event.show())
